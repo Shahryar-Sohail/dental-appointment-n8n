edb643e382dce09d1bf2ca68ea560a50589a8c63
Shahryar-Sohail <shahryar.ch123@gmail.com>
2026-03-11 22:56:07 +0500

    prefix changed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -68,7 +68,7 @@ const Index = () => {
     }
   };
   const handleSubmit = async () => {
-    const url = process.env.NEXT_PUBLIC_N8N_WEBHOOK_URL;
+    const url = import.meta.env.VITE_N8N_WEBHOOK_URL;
     setSubmitting(true);
     try {
       await fetch(url, {
